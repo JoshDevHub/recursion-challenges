dd5c7b2b149474d346d3eb9afe8006675380bd2d
Josh Smith <jmsmith1018@gmail.com>
2025-11-28 13:57:51 -0700

    Include test cases for `#fibs_rec`
    Shifts to a shared example paradigm so as to not repeat examples
    for both the iterative and recursive methods

--- spec/recursive_algorithms_spec.rb
+++ spec/recursive_algorithms_spec.rb
@@ -3,26 +3,43 @@ require "spec_helper"
 require_relative "../lib/recursive_algorithms"
 
 RSpec.describe RecursiveAlgorithms do
-  describe "::fibs" do
+  shared_examples "Fibonacci sequence" do |fibs_method|
     it "returns an empty array when passed `0`" do
-      expect(described_class.fibs(0)).to eq []
+      expected_sequence = []
+      expect(described_class.public_send(fibs_method, 0)).to eq expected_sequence
     end
 
     it "can return the first number of the sequence" do
-      expect(described_class.fibs(1)).to eq [0]
+      expected_sequence = [0]
+      expect(described_class.public_send(fibs_method, 1)).to eq expected_sequence
     end
 
     it "can return the first two numbers from the sequence" do
-      expect(described_class.fibs(2)).to eq [0, 1]
+      expected_sequence = [0, 1]
+      expect(described_class.public_send(fibs_method, 2)).to eq expected_sequence
     end
 
     it "can return the first three numbers of the sequence" do
-      expect(described_class.fibs(3)).to eq [0, 1, 1]
+      expected_sequence = [0, 1, 1]
+      expect(described_class.public_send(fibs_method, 3)).to eq expected_sequence
     end
 
     it "can return the first eight numbers of the sequence" do
       expected_sequence = [0, 1, 1, 2, 3, 5, 8, 13]
-      expect(described_class.fibs(8)).to eq expected_sequence
+      expect(described_class.public_send(fibs_method, 8)).to eq expected_sequence
     end
+
+    it "can return the first twenty numbers of the sequence" do
+      expected_sequence = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181]
+      expect(described_class.public_send(fibs_method, 20)).to eq expected_sequence
+    end
+  end
+
+  describe "::fibs" do
+    it_behaves_like "Fibonacci sequence", :fibs
+  end
+
+  describe "::fibs_rec" do
+    it_behaves_like "Fibonacci sequence", :fibs_rec
   end
 end
