a033284b62ae4ca232e5c24068d42083dac0541c
Josh Smith <jmsmith1018@gmail.com>
2025-11-28 18:36:07 -0700

    Refactor to `class << self` definition
    Allows `private` methods

--- lib/recursive_algorithms.rb
+++ lib/recursive_algorithms.rb
@@ -1,44 +1,44 @@
 module RecursiveAlgorithms
-  module_function
+  class << self
+    def fibs(num)
+      results = [0, 1]
+      return results.first(num) if results.length >= num
 
-  def fibs(num)
-    results = [0, 1]
-    return results.first(num) if results.length >= num
-
-    first, second = results
-    (num - 3).downto(0) do
-      new_element = first + second
-      first = second
-      second = new_element
-      results << new_element
+      first, second = results
+      (num - 3).downto(0) do
+        new_element = first + second
+        first = second
+        second = new_element
+        results << new_element
+      end
+      results
     end
-    results
-  end
 
-  def fibs_rec(num, sequence = [0, 1])
-    return sequence.first(num) if sequence.length >= num
+    def fibs_rec(num, sequence = [0, 1])
+      return sequence.first(num) if sequence.length >= num
 
-    sequence << (sequence[-1] + sequence[-2])
-    fibs_rec(num, sequence)
-  end
-
-  def merge_sort(array)
-    return array if array.size < 2
-
-    middle_index = array.size / 2
-    left_sort = merge_sort(array[0...middle_index])
-    right_sort = merge_sort(array[middle_index..])
-    merge(left_sort, right_sort)
-  end
-
-  private
-
-  def merge(*subarrays)
-    merged_collection = []
-    until subarrays.any?(&:empty?)
-      array_with_min_element = subarrays.min_by(&:first)
-      merged_collection << array_with_min_element.shift
+      sequence << (sequence[-1] + sequence[-2])
+      fibs_rec(num, sequence)
     end
-    merged_collection.concat(*subarrays)
+
+    def merge_sort(array)
+      return array if array.size < 2
+
+      middle_index = array.size / 2
+      left_sort = merge_sort(array[0...middle_index])
+      right_sort = merge_sort(array[middle_index..])
+      merge(left_sort, right_sort)
+    end
+
+    private
+
+    def merge(*subarrays)
+      merged_collection = []
+      until subarrays.any?(&:empty?)
+        array_with_min_element = subarrays.min_by(&:first)
+        merged_collection << array_with_min_element.shift
+      end
+      merged_collection.concat(*subarrays)
+    end
   end
 end
